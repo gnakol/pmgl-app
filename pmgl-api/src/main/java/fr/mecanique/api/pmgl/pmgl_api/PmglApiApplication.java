ebd42418cf62b461c84575f6bc4277dc77154ca9
gnakol <gna.kolie@yahoo.fr>
2025-10-28 21:37:51 +0100

    feat: initial commit for pmgl project

---
+++ pmgl-api/src/main/java/fr/mecanique/api/pmgl/pmgl_api/PmglApiApplication.java
@@ -0,0 +1,13 @@
+package fr.mecanique.api.pmgl.pmgl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmglApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmglApiApplication.class, args);
+	}
+
+}
---
+++ pmgl-api/src/test/java/fr/mecanique/api/pmgl/pmgl_api/PmglApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mecanique.api.pmgl.pmgl_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmglApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
